4abf1784336992ea1a7419866737b68f2e6ccc6d
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-15 23:25:36 +0530

    Add files via upload

---
+++ PG Accommodation/com/example/repository/PgPlaceRepository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import com.example.entity.*;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PgPlaceRepository extends JpaRepository<PgPlace, Long> {
+
+    List<PgPlace> findByCityCityIdAndAvailabilityStatus(Long cityId, String status);
+
+    List<PgPlace> findByLocalityLocalityNameIgnoreCase(String locality);
+}
+
