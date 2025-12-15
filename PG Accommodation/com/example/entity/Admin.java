a9cef1c1cc74a987d3a0c9b83db4a90bfc17c5ef
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-15 23:23:23 +0530

    Add files via upload

---
+++ PG Accommodation/com/example/entity/Admin.java
@@ -0,0 +1,19 @@
+package com.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+
+    private String name;
+    private String email;
+    private String password;
+}
+
