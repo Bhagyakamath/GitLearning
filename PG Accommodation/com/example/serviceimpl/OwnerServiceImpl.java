1696413cf9f01a3cf479590141df187486144de5
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-22 23:40:27 +0530

    Update OwnerServiceImpl.java

--- PG Accommodation/com/example/serviceimpl/OwnerServiceImpl.java
+++ PG Accommodation/com/example/serviceimpl/OwnerServiceImpl.java
@@ -18,6 +18,12 @@ public class OwnerServiceImpl implements OwnerService {
     @Autowired
     private OwnerRepository ownerRepo;
     
+    @Autowired
+    private LocalityRepository locRepo;
+    
+    @Autowired
+    private CityRepository cityRepo;
+    
     @Override
     public Owner registerOwner(Owner owner) {
 
@@ -30,10 +36,24 @@ public class OwnerServiceImpl implements OwnerService {
 
     @Override
     public PgPlace addPlace(PgPlace place) {
-
-        if (place.getOwner().getAge() < 18) {
-            throw new InvalidAgeException("Owner must be 18+");
+    	Long ownerId = place.getOwner().getOwnerId();
+    	Owner owner = ownerRepo.findById(ownerId)
+                .orElseThrow(() -> new RuntimeException("Owner not found"));
+    	
+    	Locality locality = locRepo.findById(place.getLocality().getLocalityId())
+                .orElseThrow(() -> new RuntimeException("Locality not found"));
+    	
+    	City city = cityRepo.findById(place.getCity().getCityId())
+                .orElseThrow(() -> new RuntimeException("City not found"));
+        
+        if (owner.getAge() < 18) {
+            throw new InvalidAgeException("Owner must be 18+ to add PG place");
         }
+        
+        
+        place.setOwner(owner);
+        place.setLocality(locality);
+        place.setCity(city);
         place.setAvailabilityStatus("AVAILABLE");
         place.setVisitorCount(0);
         return pgRepo.save(place);
@@ -47,24 +67,61 @@ public class OwnerServiceImpl implements OwnerService {
     }
 
     @Override
-    public PgPlace changeStatus(Long pgId) {
+    public PgPlace changeStatus(Long pgId, String status) {
         PgPlace pg = pgRepo.findById(pgId)
                 .orElseThrow(() -> new ResourceNotFoundException("PG not found"));
 
-        pg.setAvailabilityStatus(
-                pg.getAvailabilityStatus().equals("AVAILABLE") ? "OCCUPIED" : "AVAILABLE");
+        if (status == null || status.isBlank()) {
+            throw new IllegalArgumentException("Status must not be null");
+        }
+
+        if (!status.equalsIgnoreCase("AVAILABLE") &&
+            !status.equalsIgnoreCase("OCCUPIED")) {
+            throw new IllegalArgumentException("Invalid status value. Give either available or occupied");
+        }
+        
+        pg.setAvailabilityStatus(status.toUpperCase());
 
         return pgRepo.save(pg);
     }
 
     @Override
     public PgPlace editPlace(PgPlace place) {
+    	PgPlace existing = pgRepo.findById(place.getPgId())
+                .orElseThrow(() -> new RuntimeException("PG Place not found"));
+    	
+    	existing.setRegistrationNumber(place.getRegistrationNumber());
+        existing.setBuiltUpArea(place.getBuiltUpArea());
+        existing.setRent(place.getRent());
+        existing.setAvailabilityStatus(place.getAvailabilityStatus());
+        
+        if (place.getCity() != null) {
+            City city = cityRepo.findById(place.getCity().getCityId())
+                    .orElseThrow(() -> new RuntimeException("City not found"));
+            existing.setCity(city);
+        }
+
+        if (place.getLocality() != null) {
+            Locality locality = locRepo.findById(place.getLocality().getLocalityId())
+                    .orElseThrow(() -> new RuntimeException("Locality not found"));
+            existing.setLocality(locality);
+        }
+        
+        if (place.getOwner() != null) {
+            Owner owner = ownerRepo.findById(place.getOwner().getOwnerId())
+                    .orElseThrow(() -> new RuntimeException("Owner not found"));
+            existing.setOwner(owner);
+        }
+
         return pgRepo.save(place);
     }
 
     @Override
-    public void deletePlace(Long id) {
+    public String deletePlace(Long id) {
+    	pgRepo.findById(id)
+        .orElseThrow(() -> new RuntimeException("PG Place not found"));
         pgRepo.deleteById(id);
+        return "Pg deleted with id: "+id;
     }
     
     @Override
@@ -73,22 +130,29 @@ public class OwnerServiceImpl implements OwnerService {
                 .orElseThrow(() -> new ResourceNotFoundException("PG not found"));
         return pg.getVisitorCount();
     }
-    
-    @Override
-    public List<Owner> getAllOwners() {
-        return ownerRepo.findAll();
-    }
 
-    @Override
-    public Owner getOwnerById(Long id) {
-        return ownerRepo.findById(id)
+	@Override
+	public List<Owner> getAllOwners() {
+		return ownerRepo.findAll();
+	}
+
+	@Override
+	public Owner getOwnerById(Long id) {
+		Owner owner = ownerRepo.findById(id)
                 .orElseThrow(() -> new RuntimeException("Owner not found"));
-    }
+		return owner;
+	}
 
-    @Override
-    public void deleteOwner(Long id) {
-        ownerRepo.deleteById(id);
-    }
+	@Override
+	public String deleteOwner(Long id) {
+		Owner owner = ownerRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Owner not found"));
+		ownerRepo.deleteById(id);
+		return "Owner deleted with id:"+id;
+		
+	}
+    
+
 
 }
 
