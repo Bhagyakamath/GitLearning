10d59c61da1c48099681d3f2a8576eeed3098d6f
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-22 23:39:09 +0530

    Update TenantService.java

--- PG Accommodation/com/example/service/TenantService.java
+++ PG Accommodation/com/example/service/TenantService.java
@@ -2,11 +2,14 @@ package com.example.service;
 
 import java.util.List;
 
+import com.example.entity.PgPlace;
 import com.example.entity.Tenant;
 
 public interface TenantService {
 
     Tenant registerTenant(Tenant tenant);
+    
+    List<PgPlace> getPlacesByCity(Long id) throws Exception;
 
     List<Tenant> getAllTenants();
 
