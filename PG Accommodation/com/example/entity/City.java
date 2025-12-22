e45c84dd8d60ca658ad36026b5eb5bbce04f3eb7
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-22 23:34:23 +0530

    Update City.java

--- PG Accommodation/com/example/entity/City.java
+++ PG Accommodation/com/example/entity/City.java
@@ -2,13 +2,18 @@ package com.example.entity;
 
 import java.util.List;
 
+import org.hibernate.validator.constraints.UniqueElements;
+
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 public class City {
@@ -16,12 +21,14 @@ public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cityId;
-
+    
+    @Column(unique=true)
     private String cityName;
 
     // One city can have many localities
     @OneToMany(mappedBy = "city")
     @JsonManagedReference
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private List<Locality> localities;
 
 	public Long getCityId() {
