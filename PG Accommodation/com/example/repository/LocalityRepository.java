64454a0ca00d7c15caa6707e2e5876f227217543
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-22 23:36:50 +0530

    Update LocalityRepository.java

--- PG Accommodation/com/example/repository/LocalityRepository.java
+++ PG Accommodation/com/example/repository/LocalityRepository.java
@@ -1,9 +1,16 @@
 package com.example.repository;
 
 import com.example.entity.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalityRepository extends JpaRepository<Locality, Long> {
+	Optional<Locality> findByLocalityName(String locality);
+
+
 }
