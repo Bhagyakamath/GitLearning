7a0473cf459a71ed8e5d95f85b9a1075fd0d8fea
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-12-22 23:38:12 +0530

    Update LocalityService.java

--- PG Accommodation/com/example/service/LocalityService.java
+++ PG Accommodation/com/example/service/LocalityService.java
@@ -7,9 +7,11 @@ import com.example.entity.Locality;
 public interface LocalityService {
 
     Locality addLocality(Locality locality);
+    
+    List<Locality> getAllLocalities();
 
     List<Locality> getLocalitiesByCity(Long cityId);
 
-    void deleteLocality(Long id);
+    String deleteLocality(Long id);
 }
 
